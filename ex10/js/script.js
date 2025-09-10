6b7c6b0715a04e78fa98f0c11123903805296135
Laura Gabriely <laura.587523la@gmail.com>
2025-09-10 18:19:55 -0300

    Corrige o caminho das imagens das cartas nos exercícios 10 e 11

--- ex10/js/script.js
+++ ex10/js/script.js
@@ -1,6 +1,6 @@
 totalCartas = 27;
 numeroCarta = Math.floor(Math.random() * totalCartas) + 1;
-caminho = "/image/carta" + numeroCarta + ".png";
+caminho = "image/carta" + numeroCarta + ".png";
 
 document.write("<h2>Carta sorteada:</h2>");
 document.write(
--- ex11/js/script.js
+++ ex11/js/script.js
@@ -1,24 +1,24 @@
 // Sorteia 12 cartas únicas e distribui para 4 jogadores (3 cartas cada)
-var totalCartas = 27;
-var cartas = [];
-for (var i = 1; i <= totalCartas; i++) {
+totalCartas = 27;
+cartas = [];
+for (i = 1; i <= totalCartas; i++) {
   cartas.push(i);
 }
 // Embaralha as cartas
-for (var i = cartas.length - 1; i > 0; i--) {
-  var j = Math.floor(Math.random() * (i + 1));
-  var temp = cartas[i];
+for (i = cartas.length - 1; i > 0; i--) {
+  j = Math.floor(Math.random() * (i + 1));
+  temp = cartas[i];
   cartas[i] = cartas[j];
   cartas[j] = temp;
 }
 
 document.write("<h1>Jogue truco:</h1>");
 document.write("<h2 style='color:red;'>Cartas sorteadas:</h2>");
-for (var jogador = 1; jogador <= 4; jogador++) {
+for (jogador = 1; jogador <= 4; jogador++) {
   document.write("<h3>Jogador " + jogador + ":</h3>");
-  for (var c = 0; c < 3; c++) {
-    var cartaNum = cartas[(jogador - 1) * 3 + c];
-    var caminho = "/image/carta" + cartaNum + ".png";
+  for (c = 0; c < 3; c++) {
+    cartaNum = cartas[(jogador - 1) * 3 + c];
+    caminho = "image/carta" + cartaNum + ".png";
     document.write(
       '<img src="' +
         caminho +
